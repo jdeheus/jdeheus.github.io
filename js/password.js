4b7832f478e88200853b5974780dd0c1b2c7a67e
Jon De Heus <jdeheus@users.noreply.github.com>
2023-11-20 15:18:51 -0500

    Add files via upload

--- js/password.js
+++ js/password.js
@@ -1,14 +1,15 @@
 // passwordProtection.js
 
-// Define the correct password
+// Defines the correct password & prompts user for a password
 const correctPassword = "JDH2023";
-
-// Prompt the user for a password
 const userEnteredPassword = prompt("Please enter the password:");
 
-// Check if the entered password is correct
 if (userEnteredPassword === correctPassword) {
-    // Password is correct, do nothing (let the page load)
+    // Password is correct, set a cookie to remember it for 7 days
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + (7 * 24 * 60 * 60 * 1000)); // 7 days from now
+    const expires = "expires=" + expirationDate.toUTCString();
+    document.cookie = "rememberPassword=true;" + expires + ";path=/"; // Set cookie with expiration and path
 } else {
     // Password is incorrect, redirect to the previous page
     alert("Incorrect password. You will be redirected to the previous page.");
